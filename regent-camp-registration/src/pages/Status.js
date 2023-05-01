684051c3f74a1f1ea21f0e423dc979f519424309
Nathan Hino <awpertunistic@gmail.com>
2023-05-01 19:53:47 -0400

    Added Major additions to the Status Page

--- regent-camp-registration/src/pages/Status.js
+++ regent-camp-registration/src/pages/Status.js
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Status.css";
 
-function Status() {
+function Status({ students }) {
+  const [updatedStudents, setUpdatedStudents] = useState(students);
+
+  const handleRegStatusChange = (index, newRegStatus) => {
+    const updatedStudent = {
+      ...updatedStudents[index],
+      regstatus: newRegStatus,
+    };
+    const newStudents = [...updatedStudents];
+    newStudents[index] = updatedStudent;
+    setUpdatedStudents(newStudents);
+  };
+
   return (
     <div>
-      <h1>Welcome to the Status Page!</h1>
-      <p>Status Info to be implemented.</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Camper Name</th>
+            <th>Age</th>
+            <th>Grade</th>
+            <th>Contact Phone</th>
+            <th>Email</th>
+            <th>Camp Selection</th>
+            <th>Registration Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {updatedStudents &&
+            updatedStudents.map((student, index) => (
+              <tr key={index}>
+                <td>{student.campername}</td>
+                <td>{student.camperage}</td>
+                <td>{student.grade}</td>
+                <td>{student.contactphone}</td>
+                <td>{student.email}</td>
+                <td>{student.campselection}</td>
+                <td>
+                  <select
+                    value={student.regstatus}
+                    onChange={(event) =>
+                      handleRegStatusChange(index, event.target.value)
+                    }
+                  >
+                    <option value="registered">Registered</option>
+                    <option value="pending">Pending</option>
+                    <option value="cancelled">Cancelled</option>
+                  </select>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
